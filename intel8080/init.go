78d380b76b33a7b022cb56e966bb67e5586bfb3f
Dustin Bowers <dustin.uncc@gmail.com>
2020-08-23 21:29:30 -0400

    Still a long way to go

--- intel8080/init.go
+++ intel8080/init.go
@@ -33,27 +33,41 @@ type stepInfo struct{
 	opcode uint8
 }
 
-// TODO
 var instructionNames = []string{
-	"NOP", "LXI", "STAX", "INX", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-	"---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-	"---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-	"---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-
-	"MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV",
-	"MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV",
-	"MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV",
-	"MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "HLT", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV", "MOV",
-
-	"---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-	"---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-	"---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-	"---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-
-	"---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-	"---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-	"---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-	"---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
+	"nop", "lxi b,#", "stax b", "inx b", "inr b", "dcr b", "mvi b,#", "rlc",
+	"ill", "dad b", "ldax b", "dcx b", "inr c", "dcr c", "mvi c,#", "rrc",
+	"ill", "lxi d,#", "stax d", "inx d", "inr d", "dcr d", "mvi d,#", "ral",
+	"ill", "dad d", "ldax d", "dcx d", "inr e", "dcr e", "mvi e,#", "rar",
+	"ill", "lxi h,#", "shld", "inx h", "inr h", "dcr h", "mvi h,#", "daa",
+	"ill", "dad h", "lhld", "dcx h", "inr l", "dcr l", "mvi l,#", "cma",
+	"ill", "lxi sp,#","sta $", "inx sp", "inr M", "dcr M", "mvi M,#", "stc",
+	"ill", "dad sp", "lda $", "dcx sp", "inr a", "dcr a", "mvi a,#", "cmc",
+	"mov b,b", "mov b,c", "mov b,d", "mov b,e", "mov b,h", "mov b,l",
+	"mov b,M", "mov b,a", "mov c,b", "mov c,c", "mov c,d", "mov c,e",
+	"mov c,h", "mov c,l", "mov c,M", "mov c,a", "mov d,b", "mov d,c",
+	"mov d,d", "mov d,e", "mov d,h", "mov d,l", "mov d,M", "mov d,a",
+	"mov e,b", "mov e,c", "mov e,d", "mov e,e", "mov e,h", "mov e,l",
+	"mov e,M", "mov e,a", "mov h,b", "mov h,c", "mov h,d", "mov h,e",
+	"mov h,h", "mov h,l", "mov h,M", "mov h,a", "mov l,b", "mov l,c",
+	"mov l,d", "mov l,e", "mov l,h", "mov l,l", "mov l,M", "mov l,a",
+	"mov M,b", "mov M,c", "mov M,d", "mov M,e", "mov M,h", "mov M,l", "hlt",
+	"mov M,a", "mov a,b", "mov a,c", "mov a,d", "mov a,e", "mov a,h",
+	"mov a,l", "mov a,M", "mov a,a", "add b", "add c", "add d", "add e",
+	"add h", "add l", "add M", "add a", "adc b", "adc c", "adc d", "adc e",
+	"adc h", "adc l", "adc M", "adc a", "sub b", "sub c", "sub d", "sub e",
+	"sub h", "sub l", "sub M", "sub a", "sbb b", "sbb c", "sbb d", "sbb e",
+	"sbb h", "sbb l", "sbb M", "sbb a", "ana b", "ana c", "ana d", "ana e",
+	"ana h", "ana l", "ana M", "ana a", "xra b", "xra c", "xra d", "xra e",
+	"xra h", "xra l", "xra M", "xra a", "ora b", "ora c", "ora d", "ora e",
+	"ora h", "ora l", "ora M", "ora a", "cmp b", "cmp c", "cmp d", "cmp e",
+	"cmp h", "cmp l", "cmp M", "cmp a", "rnz", "pop b", "jnz $", "jmp $",
+	"cnz $", "push b", "adi #", "rst 0", "rz", "ret", "jz $", "ill", "cz $",
+	"call $", "aci #", "rst 1", "rnc", "pop d", "jnc $", "out p", "cnc $",
+	"push d", "sui #", "rst 2", "rc", "ill", "jc $", "in p", "cc $", "ill",
+	"sbi #", "rst 3", "rpo", "pop h", "jpo $", "xthl", "cpo $", "push h",
+	"ani #", "rst 4", "rpe", "pchl", "jpe $", "xchg", "cpe $", "ill", "xri #",
+	"rst 5", "rp", "pop psw", "jp $", "di", "cp $", "push psw","ori #",
+	"rst 6", "rm", "sphl", "jm $", "ei", "cm $", "ill", "cpi #", "rst 7",
 }
 
 var instructionBytes = []uint8{
@@ -79,7 +93,7 @@ func (cpu *CPU) createInstructionTable() {
 	cpu.table = []func(*stepInfo) uint{
 		cpu.nop, cpu.lxi, cpu.stax, cpu.inx, cpu.inr, cpu.dcr, cpu.mvi, cpu.rlc, nil, cpu.dad, cpu.ldax, cpu.dcx, cpu.inr, cpu.dcr, cpu.mvi, cpu.rrc,
 		nil, cpu.lxi, cpu.stax, cpu.inx, cpu.inr, cpu.dcr, cpu.mvi, cpu.ral, nil, cpu.dad, cpu.ldax, cpu.dcx, cpu.inr, cpu.dcr, cpu.mvi, cpu.rar,
-		nil, cpu.lxi, cpu.SHLD, cpu.inx, cpu.inr, cpu.dcr, cpu.mvi, cpu.daa, nil, cpu.dad, cpu.lhld, cpu.dcx, cpu.inr, cpu.dcr, cpu.mvi, cpu.cma,
+		nil, cpu.lxi, cpu.shld, cpu.inx, cpu.inr, cpu.dcr, cpu.mvi, cpu.daa, nil, cpu.dad, cpu.lhld, cpu.dcx, cpu.inr, cpu.dcr, cpu.mvi, cpu.cma,
 		nil, cpu.lxi, cpu.sta, cpu.inx, cpu.inr, cpu.dcr, cpu.mvi, cpu.stc, nil, cpu.dad, cpu.lda, cpu.dcx, cpu.inr, cpu.dcr, cpu.mvi, cpu.cmc,
 
 		cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov, cpu.mov,
@@ -132,374 +146,4 @@ func (cpu *CPU) LoadInvaders(filepath string) error {
 		}
 	}
 	return nil
-}
-
-func (cpu *CPU) add(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) sub(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) ana(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) ora(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) adc(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) sbb(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) xra(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cmp(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rnz(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rnc(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rpo(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rp(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) lxi(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) stax(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) SHLD(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) sta(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) inx(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) inr(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) dcr(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) mvi(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rlc(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) ral(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) daa(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) stc(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) dad(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) ldax(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) lhld(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) lda(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) dcx(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rrc(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rar(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cma(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cmc(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) pop(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) jnz(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) jnc(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) jpo(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) jp(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) out(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) xthl(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) di(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cnz(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cnc(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cpo(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cp(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) push(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) ori(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) adi(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) sui(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) ani(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rst(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rz(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rm(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) ret(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rc(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) rpe(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) sphl(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) jz(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) jc(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) pchl(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) jm(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) in(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) jpe(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) ei(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cz(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cc(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) xchg(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cm(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) call(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) aci(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) sbi(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cpe(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) xri(info *stepInfo) uint {
-
-	return 0
-}
-
-func (cpu *CPU) cpi(info *stepInfo) uint {
-
-	return 0
 }
--- intel8080/opcodes.go
+++ intel8080/opcodes.go
@@ -1,5 +1,6 @@
 package intel8080
 
+import "log"
 
 func (cpu *CPU) nop(info *stepInfo) uint {
 	return 4
@@ -11,38 +12,16 @@ func (cpu *CPU) hlt(info *stepInfo) uint {
 }
 
 func (cpu *CPU) mov(info *stepInfo) uint {
-	ddd, sss := opcodeDDDSSS(info.opcode)
 	var cycles uint = 5
+	ddd, sss := getOpcodeDDDSSS(info.opcode)
 
 	var destPtr, srcPtr *uint8
-	switch ddd {
-	case 0b111: destPtr = &cpu.A
-	case 0b000: destPtr = &cpu.B
-	case 0b001: destPtr = &cpu.C
-	case 0b010: destPtr = &cpu.D
-	case 0b011: destPtr = &cpu.E
-	case 0b100: destPtr = &cpu.H
-	case 0b101: destPtr = &cpu.L
-	case 0b110:
-		cycles = 7
-		memOffset := (uint16(cpu.H) << 8) | uint16(cpu.L)
-		destPtr = &cpu.Memory[memOffset]
-	}
+	destPtr, memAccess := cpu.getOpcodeRegPtr(ddd)
+	if memAccess { cycles = 7 }
+	srcPtr, memAccess = cpu.getOpcodeRegPtr(sss)
+	if memAccess { cycles = 7 }
 
-	switch sss {
-	case 0b111: srcPtr = &cpu.A
-	case 0b000: srcPtr = &cpu.B
-	case 0b001: srcPtr = &cpu.C
-	case 0b010: srcPtr = &cpu.D
-	case 0b011: srcPtr = &cpu.E
-	case 0b100: srcPtr = &cpu.H
-	case 0b101: srcPtr = &cpu.L
-	case 0b110:
-		cycles = 7
-		memOffset := (uint16(cpu.H) << 8) | uint16(cpu.L)
-		srcPtr = &cpu.Memory[memOffset]
-	}
-	*destPtr = *srcPtr // Maybe check this for nil-dereference? (it shouldn't be possible though)
+	*destPtr = *srcPtr // Maybe check this for nil-dereference? (it should be impossible, though)
 	return cycles
 }
 
@@ -53,11 +32,175 @@ func (cpu *CPU) jmp(info *stepInfo) uint {
 	return 10
 }
 
+func (cpu *CPU) adc(info *stepInfo) uint {
+
+	return 0
+}
+
+func (cpu *CPU) inr(info *stepInfo) uint {
+	var cycles uint = 5
+	ddd, _ := getOpcodeDDDSSS(info.opcode)
+	destPtr, memAccess := cpu.getOpcodeRegPtr(ddd)
+	if memAccess {
+		cycles = 10
+	}
+	*destPtr++
+
+	cpu.Sign = (*destPtr & 0b1000000) != 0
+	cpu.AuxCarry = (*destPtr & 0b1111) == 0
+	cpu.Zero = *destPtr == 0
+	cpu.Parity = getParity(*destPtr)
+	return cycles
+}
+
+func (cpu *CPU) lxi(info *stepInfo) uint {
+	lb, hb := cpu.getOpcodeArgs(info.PC)
+	rp := getOpcodeRP(info.opcode)
+
+	switch rp {
+	case 0x00:
+		cpu.B = hb
+		cpu.C = lb
+	case 0x01:
+		cpu.D = hb
+		cpu.E = lb
+	case 0x10:
+		cpu.H = hb
+		cpu.L = lb
+	case 0x11:
+		cpu.SP = (uint16(hb) << 8) | uint16(lb)
+	}
+	return 10
+}
+
+func (cpu *CPU) lda(info *stepInfo) uint {
+	lb, hb := cpu.getOpcodeArgs(info.PC)
+	address := (uint16(hb) << 8) | uint16(lb)
+	cpu.A = cpu.Memory[address]
+	return 13
+}
+
+func (cpu *CPU) sta(info *stepInfo) uint {
+	lb, hb := cpu.getOpcodeArgs(info.PC)
+	address := (uint16(hb) << 8) | uint16(lb)
+	cpu.Memory[address] = cpu.A
+	return 13
+}
+
+func (cpu *CPU) lhld(info *stepInfo) uint {
+	lb, hb := cpu.getOpcodeArgs(info.PC)
+	address := (uint16(hb) << 8) | uint16(lb)
+	cpu.L = cpu.Memory[address]
+	cpu.H = cpu.Memory[address+1]
+	return 16
+}
+
+func (cpu *CPU) shld(info *stepInfo) uint {
+	lb, hb := cpu.getOpcodeArgs(info.PC)
+	address := (uint16(hb) << 8) | uint16(lb)
+	cpu.Memory[address] = cpu.L
+	cpu.Memory[address+1] = cpu.H
+	return 16
+}
+
+func (cpu *CPU) ldax(info *stepInfo) uint {
+	var address uint16
+	rp := getOpcodeRP(info.opcode)
+	switch rp {
+	case 0x00:
+		address = (uint16(cpu.B) << 8) | uint16(cpu.C)
+	case 0x01:
+		address = (uint16(cpu.D) << 8) | uint16(cpu.E)
+	default:
+		log.Fatalf("Invalid opcode: %v", info)
+	}
+	cpu.A = cpu.Memory[address]
+	return 7
+}
+
+func (cpu *CPU) stax(info *stepInfo) uint {
+	var address uint16
+	rp := getOpcodeRP(info.opcode)
+	switch rp {
+	case 0x00:
+		address = (uint16(cpu.B) << 8) | uint16(cpu.C)
+	case 0x01:
+		address = (uint16(cpu.D) << 8) | uint16(cpu.E)
+	default:
+		log.Fatalf("Invalid opcode: %v", info)
+	}
+	cpu.Memory[address] = cpu.A
+	return 7
+}
+
+func (cpu *CPU) xchg(info *stepInfo) uint {
+	var tempD, tempE uint8
+	tempD = cpu.D
+	tempE = cpu.E
+	cpu.D = cpu.H
+	cpu.E = cpu.L
+	cpu.H = tempD
+	cpu.L = tempE
+	return 5
+}
+
+func (cpu *CPU) add(info *stepInfo) uint {
+	var cycles uint = 4
+	_, sss := getOpcodeDDDSSS(info.opcode)
+	regPtr, memAccess := cpu.getOpcodeRegPtr(sss)
+	if memAccess {
+		cycles = 7
+	}
+	result := cpu.A + *regPtr
+	cpu.Zero = result == 0
+	cpu.Sign = result & 0b10000000 != 0
+	cpu.Parity = getParity(result)
+	cpu.AuxCarry = ((cpu.A ^ result ^ *regPtr) & 0b00010000) > 0 // ?? TODO: test this
+
+	cpu.A = result
+	return cycles
+}
+
+// ----------------------------
+// -------- Helpers -----------
+
+func (cpu *CPU) getOpcodeRegPtr(regIndicator uint8) (*uint8, bool) {
+	var ptr *uint8
+	memoryAccess := false
+	switch regIndicator {
+	case 0b111: ptr = &cpu.A
+	case 0b000: ptr = &cpu.B
+	case 0b001: ptr = &cpu.C
+	case 0b010: ptr = &cpu.D
+	case 0b011: ptr = &cpu.E
+	case 0b100: ptr = &cpu.H
+	case 0b101: ptr = &cpu.L
+	case 0b110:
+		memoryAccess = true
+		memOffset := (uint16(cpu.H) << 8) | uint16(cpu.L)
+		ptr = &cpu.Memory[memOffset]
+	}
+	return ptr, memoryAccess
+}
+
 func (cpu *CPU) getOpcodeArgs(PC uint16) (byte1, byte2 uint8) {
 	return cpu.Memory[PC+1], cpu.Memory[PC+2]
 }
 
-func opcodeDDDSSS(opcode uint8) (ddd uint8, sss uint8) {
+func getParity(b uint8) bool {
+	ones := uint8(0)
+	// TODO: this could be optimized...
+	for i:=0; i < 8; i++ {
+		ones += (b >> 0) & 0b1
+	}
+	return (ones & 0b1) == 0
+}
+
+func getOpcodeRP(opcode uint8) uint8 {
+	return (opcode >> 4) & 0b11
+}
+
+func getOpcodeDDDSSS(opcode uint8) (ddd uint8, sss uint8) {
 	ddd = (opcode >> 3) & 0b111
 	sss = opcode & 0b111
 	return ddd, sss
